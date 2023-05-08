861c6ff399ed8b0975631a8c972005e70acb0012
Krishna Vishwakarma <krishnavishwakarma.og@gmail.com>
2023-05-08 11:13:28 +0530

    first commit

---
+++ test/paytm_allinonesdk_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+// import 'package:allinonesdk/allinonesdk.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('allinonesdk');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  // test('getPlatformVersion', () async {
+  //   expect(await Allinonesdk.platformVersion, '42');
+  // });
+}
